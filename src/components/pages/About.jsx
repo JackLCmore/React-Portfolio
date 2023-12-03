ee97cd107a2222e5fbe0b4e1469dd649a6bda9fc
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-03 15:26:41 -0800

    added photos to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,18 @@
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <img id="selfie" src="assets/images/selfie.jpg" alt="self photo"></img>
+      <h1>About Me</h1>
       <p>
         Hi! My name is Jack Lunchick-Seymour, I am an aspiring Web Developer currently enrolled with UC Berkeley Extension's full-stack MERN bootcamp.
       </p>
-      
+      <img id="selfie" src="assets/images/selfie.jpg"></img>
+      <img id="games" src="assets/images/Games.webp"></img>
+      <img id="magic" src="assets/images/magic.jpg"></img>
+      <img id="frisbee" src="assets/images/frisbee.png"></img>
+      <img id="friends" src="assets/images/friends.png"></img>
+      <p id="below-pics">
+        My hobbies include video games, Magic the Gathering, ultimate frisbee and most importantly spending time with my close friends.
+      </p>
     </div>
   );
 }
