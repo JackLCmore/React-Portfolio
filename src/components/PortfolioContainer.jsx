0efae6c21a5d384b1815019f77003090a1f3005f
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-03 14:06:09 -0800

    page base and conditionally rendered navbar functional

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  // const renderPage = () => {
+  //   switch (currentPage) {
+  //     case 'Home':
+  //       return <Home />;
+  //     case 'About':
+  //       return <About />;
+  //     case 'Portfolio':
+  //       return <Portfolio />;
+  //     default:
+  //       return <Contact />;
+  //   }
+  // };
+  
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main className="mx-3">{renderPage()}</main>
+    </div>
+  );
+}
