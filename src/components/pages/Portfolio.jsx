f05e7101c2d1620d88be332e69ffb065d444dae5
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-03 18:19:45 -0800

    added portfolio images and styled correspondently

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,7 +5,12 @@ export default function Portfolio() {
       <p>
         All of my work is available publicly through my gitHub, here are some of my favorite projects that I've worked on!
       </p>
-      
+      <img id="darwnism" src="assets/images/Darwnism.png"></img>
+      <img id="gonutz" src="assets/images/gonutz-pic.png"></img>
+      <img id="smarter" src="assets/images/quiz.png"></img>
+      <img id="bloggins" src="assets/images/bloggins.png"></img>
+      <img id="editor" src="assets/images/editor.png"></img>
+      <img id="jate" src="assets/images/jate.png"></img>
     </div>
   );
 }
