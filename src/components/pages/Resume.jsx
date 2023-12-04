f96eb66d7aa879345a17eea3c42d0c7a0f3e3f95
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-03 20:34:22 -0800

    added link in resume page and skills

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,8 +3,24 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <p>
-        Resume
+        For a link to my downloadable Resume, click <a href="https://docs.google.com/document/d/1dg_NRJsUIBZxLvDQt5issNcqnWnbdt-b/edit?usp=sharing&ouid=117394215640432619166&rtpof=true&sd=true">here.</a>
       </p>
+      <h2>Skills</h2>
+      <ul>
+        <li>Customer Service Excellence </li>
+        <li>Problem Solving</li>
+        <li>HTML 5</li>
+        <li>CSS</li>
+        <li>CSS frameworks (bootstrap, materialize)</li>
+        <li>Javascript</li>
+        <li>jQuery</li>
+        <li>Node js</li>
+        <li>npm</li>
+        <li>Express js</li>
+        <li>MySql</li>
+        <li>MongoDB</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 }
