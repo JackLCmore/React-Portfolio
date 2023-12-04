8340418cf706a752147ef81cdc9ec77be550d883
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-12-04 01:11:28 -0800

    styling for page headers and text

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -5,7 +5,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      <><div id="me">Jack Lunchick -Seymour</div></>
+      <><div id="me">Jack Lunchick-Seymour</div></>
       <NavTabs />
       <main className="container mx-3"><Outlet /></main>
     </div>
